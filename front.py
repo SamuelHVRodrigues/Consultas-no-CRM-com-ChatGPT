de112bcce2c8ff3554b57378e7ade1db3f3c1888
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2024-09-18 23:47:42 -0300

    Test with assistant

--- front.py
+++ front.py
@@ -8,9 +8,14 @@ question = st.text_input("Digite sua pergunta:")
 if st.button("Perguntar"):
     if question:
         response = requests.post("http://localhost:5000/ask", json={"question": question})
-        st.write("Status code:", response.status_code)
-        st.write("Resposta bruta:", response.text)  # Exibir a resposta bruta
-        try:
-            st.write("Resposta JSON:", response.json())  # Tente fazer o parsing do JSON
-        except ValueError as e:
-            st.write("Erro ao fazer o parsing do JSON:", e)
+        if response.status_code == 200:
+            data = response.json()
+            st.subheader("Resposta:")
+            st.write(data.get('answer'))
+        else:
+          st.write("Status code:", response.status_code)
+          st.write("Resposta bruta:", response.text)  # Exibir a resposta bruta
+          try:
+              st.write("Resposta JSON:", response.json())  # Tente fazer o parsing do JSON
+          except ValueError as e:
+              st.write("Erro ao fazer o parsing do JSON:", e)
