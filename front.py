7ad58a8169c3b856032919a15dd10df4eae5f0c8
Samuel Rodrigues <73417269+SamuelHVRodrigues@users.noreply.github.com>
2024-09-21 03:52:29 -0300

    add server url

--- front.py
+++ front.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import requests
+from dotenv import load_dotenv, find_dotenv, dotenv_values
+
+# Carregar variáveis de ambiente
+_ = load_dotenv(find_dotenv())
+env_vars = dotenv_values(find_dotenv())
+server_url = env_vars.get('SERVER_URL')
 
 st.title("Aplicação com OpenAI no Backend")
 
@@ -7,15 +13,15 @@ question = st.text_input("Digite sua pergunta:")
 
 if st.button("Perguntar"):
     if question:
-        response = requests.post("http://localhost:5000/ask", json={"question": question})
+        response = requests.post(server_url, json={"question": question})
         if response.status_code == 200:
             data = response.json()
             st.subheader("Resposta:")
             st.write(data.get('answer'))
         else:
           st.write("Status code:", response.status_code)
-          st.write("Resposta bruta:", response.text)  # Exibir a resposta bruta
+          st.write("Resposta bruta:", response.text)
           try:
-              st.write("Resposta JSON:", response.json())  # Tente fazer o parsing do JSON
+              st.write("Resposta JSON:", response.json())
           except ValueError as e:
               st.write("Erro ao fazer o parsing do JSON:", e)
