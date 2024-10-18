cf2b824ef153e39cd572cd0a5679cb6cf15be19d
Matheus Augusto Alves Bonfim <115117550+matheusbon@users.noreply.github.com>
2024-10-18 17:39:37 -0300

    Update 2_📈_Desempenho_Vendedores.py

--- pages/2_📈_Desempenho_Vendedores.py
+++ pages/2_📈_Desempenho_Vendedores.py
@@ -17,7 +17,7 @@ st.set_page_config(layout="wide",
                    initial_sidebar_state="collapsed")
 
 # Para poder ler o arquivo CSS para customizar a página
-with open("assets\style.css") as f:
+with open("assets/style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
